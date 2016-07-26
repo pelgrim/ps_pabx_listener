427b5a14077a038d29bba677a5174cad43751e77
Lucas Vieira <lucas@vieira.io>
2016-07-26 14:03:06 -0300

    main spec sketch

---
+++ spec/main_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe PsPabxListener::Main do
+  it 'requires a host'
+  it 'requires a username'
+  it 'requires a password'
+  it 'creates a valid instance'
+  it 'gets data until a given timeout is reached'
+  it 'continuously gets data until its required to stop'
+end
