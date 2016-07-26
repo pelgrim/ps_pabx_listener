179314e32a14f3dc2261ec0e930915ae900c0069
Lucas Vieira <lucas@vieira.io>
2016-07-26 14:02:10 -0300

    listener spec sketch

---
+++ spec/listener_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe PsPabxListener::Listener do
+  it 'requires a host'
+  it 'requires a username'
+  it 'requires a password'
+  it 'accepts a timeout value'
+  it 'connects to a PABX'
+  it 'disconnects from a PABX'
+  it 'discards irrelevant information'
+end
