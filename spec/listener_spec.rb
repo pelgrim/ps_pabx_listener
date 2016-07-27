b61a27b8a400ea48ea167edcdabb1eb23e288bee
Lucas Vieira <lucas@vieira.io>
2016-07-27 10:39:36 -0300

    listener spec tests telnet method

--- spec/listener_spec.rb
+++ spec/listener_spec.rb
@@ -45,22 +45,3 @@ describe PsPabxListener::Listener do
   end
 
 end
-
-def prepare_telnet timeout=false
-  telnet = double('telnet', null_object: true)
-  allow(telnet).to receive(:close).and_return(:true)
-  allow(telnet).to receive(:waitfor).
-    with(/(-|Rejected)/).
-    and_return('-')
-  allow(telnet).to receive(:cmd).
-    with({"String" => "john", "Match" => /[Pp]ass(?:word|phrase)[:]*\z/n}).
-    and_return("Trash\n")
-  allow(telnet).to receive(:cmd).
-    with('foo').and_return("10/10/10 not trash\n")
-  allow(Net::Telnet).to receive(:new).with(
-    'Host' => 'localhost',
-    'Port' => 2300,
-    'Prompt' => /(\n|\r)/,
-    'Timeout' => timeout,
-    'Waittime' => timeout).and_return(telnet)
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,21 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ps_pabx_listener'
+
+def prepare_telnet timeout=false
+  telnet = double('telnet', null_object: true)
+  allow(telnet).to receive(:close).and_return(:true)
+  allow(telnet).to receive(:waitfor).
+    with(/(-|Rejected)/).
+    and_return('-')
+  allow(telnet).to receive(:cmd).
+    with({"String" => "john", "Match" => /[Pp]ass(?:word|phrase)[:]*\z/n}).
+    and_return("Trash\n")
+  allow(telnet).to receive(:cmd).
+    with('foo').and_return("10/10/10 not trash\n")
+  allow(Net::Telnet).to receive(:new).with(
+    'Host' => 'localhost',
+    'Port' => 2300,
+    'Prompt' => /(\n|\r)/,
+    'Timeout' => timeout,
+    'Waittime' => timeout).and_return(telnet)
+end
